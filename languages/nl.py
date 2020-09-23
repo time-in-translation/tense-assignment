c758d2745a96198dd0fd8498fb6891fa8061f515
Martijn van der Klis <M.H.vanderKlis@uu.nl>
2020-09-23 12:57:47 +0200

    Some more playing around with automatic tense assignment

--- languages/nl.py
+++ languages/nl.py
@@ -11,12 +11,16 @@ def get_tense_nl(words, pos, lemmata):
     tense = ''
 
     if len(pos) == 1:
-        if pos[0] in ['verbpressg', 'verbprespl']:
+        if pos[0] in ['verbpressg', 'verbprespl', 'verbinf']:
             tense = 'ott'
-        if pos[0] in ['verbpastsg', 'verbpastpl', 'verbpapa']:
+        if pos[0] in ['verbpastsg', 'verbpastpl']:
             tense = 'ovt'
+        if pos[0] in ['verbpapa']:
+            tense = 'vtt'
     if len(pos) == 2:
         if set(pos) == {'verbpressg', 'verbinf'} or set(pos) == {'verbprespl', 'verbinf'}:
+            tense = 'ott'
+        if set(pos) == {'verbpressg', 'adj'} or set(pos) == {'verbprespl', 'adj'}:
             tense = 'ott'
 
         if set(pos) == {'verbpressg', 'verbpapa'} or set(pos) == {'verbprespl', 'verbpapa'}:
@@ -30,6 +34,8 @@ def get_tense_nl(words, pos, lemmata):
             tense = 'ovtt'
     if len(pos) == 3:
         if set(pos) == {'pronrefl', 'verbpressg', 'verbpapa'} or set(pos) == {'pronrefl', 'verbprespl', 'verbpapa'}:
+            tense = 'vtt'
+        if set(pos) == {'verbpressg', 'verbinf', 'verbinf'}:
             tense = 'vtt'
         if set(pos) == {'pronrefl', 'verbpastsg', 'verbpapa'} or set(pos) == {'pronrefl', 'verbpastpl', 'verbpapa'}:
             tense = 'vvt'
--- process.py
+++ process.py
@@ -6,35 +6,52 @@ from collections import Counter
 
 import numpy as np
 from sklearn import tree, preprocessing
+from sklearn.metrics import confusion_matrix, f1_score
 from sklearn.model_selection import cross_val_score
+from pattern.text.es import tenses as tenses_es
+from pattern.text.nl import tenses as tenses_nl
 
 from languages.es import get_tense_es
 from languages.nl import get_tense_nl
 
 
 class Annotation(object):
-    def __init__(self, id, target, words, pos, lemmata):
-        self.id = id
+    def __init__(self, language, identifier, target, words, pos, lemmata):
+        self.identifier = identifier
         self.target = target
         self.words = list(filter(None, words))
         self.pos = list(filter(None, pos))
         self.lemmata = list(filter(None, lemmata))
+        self.unf_words = words
         self.unf_pos = pos
         self.unf_lemmata = lemmata
+        tense_func = tenses_es if language == 'es' else tenses_nl
+        tenses = [tense_func(w) if w else '' for w in words]
+        # For all features below, we take the first given tense classification by Pattern
+        self.tense = [xstr(t[0][0]) if t else '' for t in tenses]
+        self.person = [xstr(t[0][1]) if t else '' for t in tenses]
+        self.number = [xstr(t[0][2]) if t else '' for t in tenses]
+        self.mood = [xstr(t[0][3]) if t else '' for t in tenses]
+        self.aspect = [xstr(t[0][4]) if t else '' for t in tenses]
 
     def to_array(self):
-        return np.array(self.unf_pos + self.unf_lemmata)
+        return np.array(self.unf_words + self.unf_pos + self.unf_lemmata +
+                        self.tense + self.person + self.number + self.mood + self.aspect)
 
 
-def import_csv(filename):
+def xstr(s):
+    return '' if s is None else str(s)
+
+
+def import_csv(language, filename):
     result = []
 
-    with open(filename, 'r') as f:
+    with open(filename, 'r') as handler:
         word_columns = []
         pos_columns = []
         lemmata_columns = []
 
-        reader = csv.reader(f, delimiter=';')
+        reader = csv.reader(handler, delimiter=';')
         for i, row in enumerate(reader):
             if i == 0:  # header row
                 for j, r in enumerate(row):
@@ -45,12 +62,12 @@ def import_csv(filename):
                     if r.startswith('lem'):
                         lemmata_columns.append(j)
             else:  # other rows
-                id = int(row[0])
+                identifier = int(row[0])
                 target = row[1]
                 words = [row[c] for c in word_columns]
                 pos = [row[c] for c in pos_columns]
                 lemmata = [row[c] for c in lemmata_columns]
-                result.append(Annotation(id, target, words, pos, lemmata))
+                result.append(Annotation(language, identifier, target, words, pos, lemmata))
 
     return result
 
@@ -87,14 +104,24 @@ if __name__ == "__main__":
     parser.add_argument('filename', type=str)
     args = parser.parse_args()
 
-    annotations = import_csv(args.filename)
-    results = assign_tenses(annotations, language=args.language)
-    show_differences(annotations, results)
+    annotations = import_csv(args.language, args.filename)
 
     X = np.array([a.to_array() for a in annotations])
     y = np.array([a.target for a in annotations])
-    print(Counter(y))
 
+    counter = Counter(y)
+    print(counter)
+
+    # Naive classifier
+    results = assign_tenses(annotations, language=args.language)
+    y_prediction = np.array(results)
+    sorted_keys = [t for t, _ in counter.most_common()]
+    print(sorted_keys)
+    print(confusion_matrix(y, y_prediction, labels=sorted_keys))
+    print(f1_score(y, y_prediction, labels=sorted_keys, average='micro'))
+    # show_differences(annotations, results)
+
+    # DecisionTreeClassifier
     X_labeling = dict()
     i = j = 0
     for n, column in enumerate(X.T):
@@ -118,12 +145,8 @@ if __name__ == "__main__":
     graph.render('test')
 
     print(clf.score(X_OHC, y))
-
     print(cross_val_score(clf, X_OHC, y, cv=5))
 
     for f in clf.tree_.feature:
         if f in X_labeling:
             print(f, X_labeling[f])
-
-    # print results
-
